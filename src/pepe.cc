d6e3d7223da82ff21de43a6ed8b4be09949e6c1a
Kyrylo Chvanov <alu0101640440@ull.edu.es>
2024-01-20 10:47:19 +0000

    Add pepe's sprite and change the dragging behavior

--- src/pepe.cc
+++ src/pepe.cc
@@ -1,26 +1,44 @@
+#define DEBUG
+
 #include <SFML/Window.hpp>
 #include <raylib.h>
 
-constexpr int kWinWidth = 200;
-constexpr int kWinHeight = 200;
+#ifdef DEBUG
+#include <iostream>
+#endif
 
 int main() {
+
+  Image pepe_sprite = LoadImage("sprites/pepe.png");
+  const int kWinWidth = pepe_sprite.width;
+  const int kWinHeight = pepe_sprite.height;
+
   SetConfigFlags(FLAG_WINDOW_TRANSPARENT | FLAG_WINDOW_UNDECORATED);
   InitWindow(kWinWidth, kWinHeight, "Pepe");
   SetTargetFPS(60);
 
+  Texture2D pepe = LoadTextureFromImage(pepe_sprite);
+  UnloadImage(pepe_sprite);
+
+  bool IsBeingDragged = false;
+
   while (!WindowShouldClose()) {
     if (IsMouseButtonDown(MOUSE_LEFT_BUTTON) && IsCursorOnScreen()) {
+      IsBeingDragged = true;
+    } else if (!IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
+      IsBeingDragged = false;
+    }
+    if (IsBeingDragged) {
       sf::Vector2i mouse_pos = sf::Mouse::getPosition();
       SetWindowPosition(mouse_pos.x - kWinWidth / 2, mouse_pos.y - kWinHeight / 2);
     }
-
     BeginDrawing();
       ClearBackground(BLANK);
-      DrawCircle(kWinWidth / 2, kWinHeight / 2, 100, GREEN);
+      DrawTexture(pepe, 0, 0, WHITE);
     EndDrawing();
   }
 
+  UnloadTexture(pepe);
   CloseWindow();
   return 0;
 }
