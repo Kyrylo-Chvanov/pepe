452d16cb482b75a7d673048fdf19ad523b45fc4e
Kyrylo Chvanov <alu0101640440@ull.edu.es>
2025-05-03 12:58:52 +0100

    Add states to Pepe

--- src/pepe.h
+++ src/pepe.h
@@ -4,6 +4,15 @@
 
 class Pepe {
  public:
+  enum State {
+    WANDERING,
+    GRABBED,
+  };
+  Pepe() : state_{WANDERING} {}
   void Draw() const;
   void Update();
+  State GetState() const { return state_; }
+  void SetState(const State state) { state_ = state; }
+ private:
+  State state_;
 };
