df14bf9c4fe42570a9569e10985524da16b4ed7b
Kyrylo Chvanov <alu0101640440@ull.edu.es>
2025-05-04 19:17:02 +0100

    Add new animations and states

--- src/pepe.cc
+++ src/pepe.cc
@@ -8,8 +8,10 @@ Pepe::Pepe()
     : state_{WANDERING},
       texture_{LoadTexture("../assets/sprites/pepe/pepe.png")},
       animations_{texture_, PEPE_FRAME_SIZE} {
-  animations_.AddAnimation("idle", Animation{1, 0, 0, 1});
-  animations_.Play("idle");
+  animations_.AddAnimation("wander", Animation{1, 0, 0, 0});
+  animations_.AddAnimation("blink", Animation{1, 1, 0, 0});
+  animations_.AddAnimation("focus", Animation{1, 2, 0, 0});
+  animations_.AddAnimation("grabbed", Animation{5, 3, 0, 1});
 }
 
 void Pepe::Draw() const {
@@ -19,4 +21,19 @@ void Pepe::Draw() const {
                                static_cast<float>(win_height)});
 }
 
-void Pepe::Update() { animations_.Update(); }
+void Pepe::ProcessState() {
+  if (state_ == State::FOCUS) {
+    animations_.Play("focus");
+  } else if (state_ == State::WANDERING) {
+    animations_.Play("wander");
+  } else if (state_ == State::BLINKING) {
+    animations_.Play("blink");
+  } else if (state_ == State::GRABBED) {
+    animations_.Play("grabbed");
+  }
+}
+
+void Pepe::Update() {
+  animations_.Update();
+  ProcessState();
+}
--- src/pepe.h
+++ src/pepe.h
@@ -1,15 +1,18 @@
 #pragma once
 
 #include <raylib.h>
+
 #include "animation.h"
 
-constexpr FrameSize PEPE_FRAME_SIZE{16, 16}; 
+constexpr FrameSize PEPE_FRAME_SIZE{16, 16};
 
 class Pepe {
  public:
   enum State {
     WANDERING,
     GRABBED,
+    BLINKING,
+    FOCUS,
   };
   Pepe();
   ~Pepe() { UnloadTexture(texture_); }
@@ -17,7 +20,12 @@ class Pepe {
   void Update();
   State GetState() const { return state_; }
   void SetState(const State state) { state_ = state; }
+  void FlipPepe(const bool flip) {
+    animations_.FlipAnimation(flip);
+  }
+
  private:
+  void ProcessState();
   State state_;
   Texture2D texture_;
   AnimationPlayer animations_;
